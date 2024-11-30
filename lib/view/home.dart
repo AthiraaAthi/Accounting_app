e1991b874188c0dd13f16ce13281f23d8315c7a9
AthiraaAthi <athiraa2612@gmail.com>
2024-11-30 11:12:08 +0530

    added body as center

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -5,6 +5,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(),
+    );
   }
 }
