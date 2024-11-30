791679ff1722ecd617cd8b09a43f0963bc6bf6a4
AthiraaAthi <athiraa2612@gmail.com>
2024-11-30 11:10:42 +0530

    added listscreen

--- lib/view/listscreen.dart
+++ lib/view/listscreen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text("List Screen")],
+        ),
+      ),
+    );
+  }
+}
