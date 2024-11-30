8eadc5fe13b86be38d0088ad8b6eabe244942ed2
AthiraaAthi <athiraa2612@gmail.com>
2024-11-30 11:11:14 +0530

    added settings screen

--- lib/view/settings_screen.dart
+++ lib/view/settings_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text("Settings Screen")],
+        ),
+      ),
+    );
+  }
+}
