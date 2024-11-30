00a10a48afd67cc0d7d5f02025f8e8f198ad41eb
AthiraaAthi <athiraa2612@gmail.com>
2024-11-30 11:12:46 +0530

    added column as the child of center

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -6,7 +6,9 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(),
+      body: Center(
+        child: Column(),
+      ),
     );
   }
 }
