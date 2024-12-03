7bb4e8564fed55f2255ac3a94253e58406689f0b
AthiraaAthi <athiraa2612@gmail.com>
2024-12-03 11:21:00 +0530

    added border radius to phone field

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -111,7 +111,8 @@ class Home extends StatelessWidget {
                                 decoration: InputDecoration(
                                   labelText: "Phone (Optional)",
                                   labelStyle: TextStyle(fontSize: 15),
-                                  border: OutlineInputBorder(),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10)),
                                 ),
                               ),
                             ),
