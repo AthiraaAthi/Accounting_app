b0ba4b499a372c2c63f88db4cb488140bbb9ed27
H4r1 <harigovindappu4926@gmail.com>
2024-12-04 16:22:11 +0530

    removed old calander

--- lib/view/listscreen.dart
+++ lib/view/listscreen.dart
@@ -1,7 +1,6 @@
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 
 import 'package:flutter/material.dart';
-import 'package:horizontal_week_calendar/horizontal_week_calendar.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -32,37 +31,8 @@ class _ListScreenState extends State<ListScreen> {
               ))
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            HorizontalWeekCalendar(
-              minDate: DateTime(2020, 1, 31),
-              maxDate: DateTime(2024, 12, 31),
-              initialDate: DateTime.now(),
-              onDateChange: (date) {
-                setState(
-                  () {
-                    selectedDate = date;
-                  },
-                );
-              },
-              showTopNavbar: true,
-              monthFormat: "MMMM yyyy",
-              showNavigationButtons: false,
-              weekStartFrom: WeekStartFrom.Monday,
-              borderRadius: BorderRadius.circular(7),
-              activeBackgroundColor: primaryColorBlue,
-              activeTextColor: Colors.white,
-              inactiveBackgroundColor: primaryColorBlue.withOpacity(.3),
-              inactiveTextColor: Colors.white,
-              disabledTextColor: Colors.grey,
-              disabledBackgroundColor: Colors.grey.withOpacity(.3),
-              activeNavigatorColor: primaryColorBlue,
-              inactiveNavigatorColor: Colors.grey,
-              monthColor: primaryColorBlue,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [],
       ),
     );
   }
