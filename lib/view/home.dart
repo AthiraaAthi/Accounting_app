3e6154f512b00fcbce346f3f24775151453970e7
H4r1 <harigovindappu4926@gmail.com>
2024-12-04 16:26:07 +0530

    modified tabbar inside dialog

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,9 +1,17 @@
-import 'package:curved_nav/view/utils/AddCardPage/add_card_screen.dart';
+import 'dart:developer';
+
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController _tabController = TabController(length: 2, vsync: this);
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +26,242 @@ class Home extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                Navigator.of(context).push(PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      const AddCardPage(),
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    var tween = Tween(
-                      begin: const Offset(1.0, 0.0),
-                      end: Offset.zero,
-                    ).chain(CurveTween(curve: Curves.easeIn));
-                    return SlideTransition(
-                      position: animation.drive(tween),
-                      child: child,
+                // Navigator.of(context).push(PageRouteBuilder(
+                //   pageBuilder: (context, animation, secondaryAnimation) =>
+                //       const AddCardPage(),
+                //   transitionsBuilder:
+                //       (context, animation, secondaryAnimation, child) {
+                //     var tween = Tween(
+                //       begin: const Offset(1.0, 0.0),
+                //       end: Offset.zero,
+                //     ).chain(CurveTween(curve: Curves.easeIn));
+                //     return SlideTransition(
+                //       position: animation.drive(tween),
+                //       child: child,
+                //     );
+                //},
+                //));
+
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    bool isSelected = true;
+                    return AlertDialog(
+                      title: Center(
+                        child: Text('Add new card'),
+                      ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          DefaultTabController(
+                            initialIndex: 0,
+                            length: 2,
+                            child: Column(
+                              children: [
+                                TabBar(
+                                  controller: _tabController,
+                                  indicatorSize: TabBarIndicatorSize.tab,
+                                  indicatorColor: Colors.blue,
+                                  indicatorWeight: 2.0,
+                                  labelColor: Colors.black,
+                                  unselectedLabelColor: Colors.grey,
+                                  tabs: [
+                                    Tab(text: "Create"),
+                                    Tab(text: "Join"),
+                                  ],
+                                ),
+                                StatefulBuilder(
+                                  builder: (context, setState) {
+                                    _tabController.addListener(() {
+                                      if (_tabController.index == 0) {
+                                        setState(
+                                          () {
+                                            isSelected = true;
+                                            log(isSelected.toString());
+                                          },
+                                        );
+                                      } else if (_tabController.index == 1) {
+                                        setState(
+                                          () {
+                                            isSelected = false;
+                                            log(isSelected.toString());
+                                          },
+                                        );
+                                      }
+                                    });
+
+                                    return AnimatedContainer(
+                                      duration: Duration(milliseconds: 200),
+                                      height: isSelected ? 600 : 300,
+                                      width: 600,
+                                      child: TabBarView(
+                                        controller: _tabController,
+                                        children: [
+                                          Column(children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Text(
+                                                  "Money Lender",
+                                                ),
+                                                Switch(
+                                                  activeColor:
+                                                      ColorConstant.defBlue,
+                                                  value: true,
+                                                  thumbColor:
+                                                      WidgetStatePropertyAll(
+                                                          Colors.white),
+                                                  inactiveThumbColor:
+                                                      ColorConstant.defBlue,
+                                                  focusColor:
+                                                      ColorConstant.defBlue,
+                                                  trackColor:
+                                                      WidgetStatePropertyAll(
+                                                    ColorConstant.defBlue,
+                                                  ),
+                                                  onChanged: (value) {},
+                                                ),
+                                                Text(
+                                                  "To Give",
+                                                ),
+                                              ],
+                                            ),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                labelText: "Name",
+                                                labelStyle:
+                                                    TextStyle(fontSize: 15),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                            ),
+                                            SizedBox(height: 15),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                labelText: "Phone (Optional)",
+                                                labelStyle:
+                                                    TextStyle(fontSize: 15),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                            ),
+                                            SizedBox(height: 15),
+                                            TextField(
+                                              maxLines: 4,
+                                              decoration: InputDecoration(
+                                                contentPadding: EdgeInsets.only(
+                                                    left: 50, top: 30),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                labelText:
+                                                    "Description (Optional)",
+                                                hintStyle:
+                                                    TextStyle(fontSize: 15),
+                                              ),
+                                            ),
+                                            SizedBox(height: 15),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                labelText:
+                                                    "Amount (With interest if any)",
+                                                labelStyle:
+                                                    TextStyle(fontSize: 15),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                              keyboardType:
+                                                  TextInputType.number,
+                                            ),
+                                            SizedBox(height: 15),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                labelText: "Installment Amount",
+                                                labelStyle:
+                                                    TextStyle(fontSize: 15),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                              keyboardType:
+                                                  TextInputType.number,
+                                            ),
+                                            SizedBox(height: 15),
+                                            DropdownButtonFormField<String>(
+                                              decoration: InputDecoration(
+                                                labelText: "Installment Type",
+                                                labelStyle:
+                                                    TextStyle(fontSize: 15),
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                              items: const [
+                                                DropdownMenuItem(
+                                                  value: "Monthly",
+                                                  child: Text("Monthly"),
+                                                ),
+                                                DropdownMenuItem(
+                                                  value: "Weekly",
+                                                  child: Text("Weekly"),
+                                                ),
+                                              ],
+                                              onChanged: (value) {},
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: const Text(
+                                                    "Cancel",
+                                                    style: TextStyle(
+                                                        color: Colors.black),
+                                                  ),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text(
+                                                    "Create",
+                                                    style: TextStyle(
+                                                      color:
+                                                          ColorConstant.defBlue,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          ]),
+                                          SizedBox(height: 100, child: Column())
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     );
                   },
-                ));
+                );
               },
               icon: Icon(
                 Icons.add_box_outlined,
@@ -47,5 +276,11 @@ class Home extends StatelessWidget {
         children: [],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
