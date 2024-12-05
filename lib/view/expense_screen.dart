3852d125b0d90327afc76c5998d21b95937c1633
AthiraaAthi <athiraa2612@gmail.com>
2024-12-05 09:12:47 +0530

    added appbar

--- lib/view/expense_screen.dart
+++ lib/view/expense_screen.dart
@@ -5,6 +5,8 @@ class ExpenseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
