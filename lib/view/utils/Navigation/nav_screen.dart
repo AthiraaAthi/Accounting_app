17b6b4698a2c7d59c3bee90378045c185704356c
H4r1 <harigovindappu4926@gmail.com>
2024-12-05 11:53:41 +0530

    added new navigation

--- lib/view/utils/Navigation/nav_screen.dart
+++ lib/view/utils/Navigation/nav_screen.dart
@@ -2,7 +2,7 @@ import 'package:curved_nav/view/utils/Home/home.dart';
 import 'package:curved_nav/view/utils/Expense/expense_screen.dart';
 import 'package:curved_nav/view/utils/Settings/settings_screen.dart';
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class NavScreen extends StatefulWidget {
@@ -13,7 +13,7 @@ class NavScreen extends StatefulWidget {
 }
 
 class _NavScreenState extends State<NavScreen> {
-  int selectedIndex = 0;
+  int selectedIndex = 1;
   List<Widget> screens = const [
     ListScreen(),
     Home(),
@@ -23,32 +23,35 @@ class _NavScreenState extends State<NavScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: screens[selectedIndex],
-      bottomNavigationBar: CurvedNavigationBar(
-          height: 65,
-          index: selectedIndex,
-          onTap: (index) {
-            selectedIndex = index;
-            setState(() {});
-          },
-          color: Colors.white,
-          backgroundColor: ColorConstant.defBlue,
-          items: [
-            Icon(
-              Icons.list,
-              color: ColorConstant.defBlue,
-              size: 30,
-            ),
-            Icon(
-              Icons.home_outlined,
-              color: ColorConstant.defBlue,
-              size: 30,
-            ),
-            Icon(
-              Icons.settings,
-              color: ColorConstant.defBlue,
-              size: 30,
-            ),
-          ]),
+      bottomNavigationBar: GNav(
+        onTabChange: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        tabBorderRadius: 15,
+        gap: 8,
+        selectedIndex: selectedIndex,
+        tabs: [
+          GButton(
+            icon: Icons.calculate_outlined,
+            text: 'Expense',
+            textColor: primaryColorBlue,
+            iconActiveColor: primaryColorBlue,
+            backgroundColor: primaryColorBlue.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(20),
+            padding: EdgeInsets.all(10),
+          ),
+          GButton(
+            icon: Icons.home_outlined,
+            text: 'home',
+          ),
+          GButton(
+            icon: Icons.settings_outlined,
+            text: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
