387c02e9964dacfd0204e1996e7597de3983c7f7
AthiraaAthi <athiraa2612@gmail.com>
2024-12-06 13:25:03 +0530

    added title to new "add category" screen

--- lib/view/utils/Expense/add_category.dart
+++ lib/view/utils/Expense/add_category.dart
@@ -1,3 +1,4 @@
+import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
 class AddCategory extends StatelessWidget {
@@ -5,6 +6,23 @@ class AddCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add Category",
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.keyboard_arrow_left,
+              size: 30,
+              color: Colors.white,
+            )),
+        backgroundColor: ColorConstant.defBlue,
+      ),
+    );
   }
 }
