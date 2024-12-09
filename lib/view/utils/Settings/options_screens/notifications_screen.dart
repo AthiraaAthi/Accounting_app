7dbd159c8f9c79fa51eb7c22ca70a8fb6b4369b3
AthiraaAthi <athiraa2612@gmail.com>
2024-12-09 13:53:20 +0530

    added toggle buttons for notifications screen and completed notification screen

--- lib/view/utils/Settings/options_screens/notifications_screen.dart
+++ lib/view/utils/Settings/options_screens/notifications_screen.dart
@@ -1,8 +1,16 @@
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
-class NotificationsScreen extends StatelessWidget {
+class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
+
+  @override
+  State<NotificationsScreen> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  bool isGeneralNotificationOn = true;
+  bool isDueReminderOn = true;
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +32,57 @@ class NotificationsScreen extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                'General Notification',
-              ),
-            ],
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'General Notification',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Switch(
+                  activeTrackColor: ColorConstant.defBlue,
+                  thumbColor: WidgetStatePropertyAll(Colors.white),
+                  inactiveTrackColor: Colors.grey,
+                  value: isGeneralNotificationOn,
+                  onChanged: (value) {
+                    setState(() {
+                      isGeneralNotificationOn = value;
+                    });
+                  },
+                )
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Due Reminder',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Switch(
+                  activeTrackColor: ColorConstant.defBlue,
+                  thumbColor: WidgetStatePropertyAll(Colors.white),
+                  inactiveTrackColor: Colors.grey,
+                  value: isGeneralNotificationOn,
+                  onChanged: (value) {
+                    setState(() {
+                      isGeneralNotificationOn = value;
+                    });
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
