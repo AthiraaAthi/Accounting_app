0c4bbf8432415bb8f99eb29d7e9fe2944ac12511
AthiraaAthi <athiraa2612@gmail.com>
2024-12-14 12:40:55 +0530

    added title

--- lib/view/utils/Home/selection_card/selection_card.dart
+++ lib/view/utils/Home/selection_card/selection_card.dart
@@ -119,7 +119,9 @@ class SelectionCard extends StatelessWidget {
               onPressed: () {
                 showDialog(
                   context: context,
-                  builder: (context) => AlertDialog(),
+                  builder: (context) => AlertDialog(
+                    title: Text("Enter Amount"),
+                  ),
                 );
               },
               child: Text("Add Payment")),
