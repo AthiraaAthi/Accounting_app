2225e369ee4f5bf0d06e33007779a0ddeabbe9fb
H4r1 <harigovindappu4926@gmail.com>
2024-12-15 17:32:00 +0530

    modified popUpMenu

--- lib/view/utils/Home/Widgets/popUpMenu.dart
+++ lib/view/utils/Home/Widgets/popUpMenu.dart
@@ -1,3 +1,4 @@
+import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
 class MenuButtonWidget extends StatelessWidget {
@@ -8,24 +9,137 @@ class MenuButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<String>(
+      color: white,
       icon: const Icon(Icons.more_vert),
-      onSelected: (String value) {},
+      onSelected: (String value) {
+        if (value == '1') {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return AlertDialog(
+                backgroundColor: white,
+                title: Center(child: Text("Enter Amount")),
+                content: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.remove_circle_outline,
+                              size: 38,
+                              color: Colors.grey,
+                            ))),
+                    Expanded(
+                        flex: 6,
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          cursorColor: primaryColorBlue,
+                          maxLength: 6,
+                          style: TextStyle(fontSize: 35),
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(top: 15),
+                              counterText: ' ',
+                              hintText: '0',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent))),
+                        )),
+                    Expanded(
+                        flex: 2,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.add_circle_outline,
+                              size: 38,
+                              color: Colors.grey,
+                            ))),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel')),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Add',
+                        style: TextStyle(color: primaryColorBlue),
+                      )),
+                ],
+              );
+            },
+          );
+        } else if (value == '3') {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                contentPadding: EdgeInsets.symmetric(vertical: 80),
+                backgroundColor: white,
+                content: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '(TYPE-ANYTHING)',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.copy_outlined,
+                              color: black,
+                            )),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.share_outlined,
+                              color: black,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
       itemBuilder: (BuildContext context) {
         return [
           PopupMenuItem(
-            value: 'Add Amount',
+            value: '1',
             child: Text('Add Amount'),
           ),
           PopupMenuItem(
-            value: 'Delete Account',
+            value: '2',
             child: Text('Delete Account'),
           ),
           PopupMenuItem(
-            value: 'Share',
+            value: '3',
             child: Text('Share'),
           ),
           PopupMenuItem(
-            value: 'Help',
+            value: '4',
             child: Text('Help'),
           ),
         ];
