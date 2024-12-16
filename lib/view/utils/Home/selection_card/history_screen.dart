78128eee1fa50b11d2733eb6d1790ad1a7013761
AthiraaAthi <athiraa2612@gmail.com>
2024-12-16 11:29:55 +0530

    added total balance in the history screen

--- lib/view/utils/Home/selection_card/history_screen.dart
+++ lib/view/utils/Home/selection_card/history_screen.dart
@@ -7,34 +7,67 @@ class HistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.keyboard_arrow_left,
+              size: 30,
+            )),
+        backgroundColor: primaryColorBlue,
+        foregroundColor: white,
+        title: Text("History"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 8,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                  child: ListTile(
+                    subtitle: Text("Balance Amount:1000"),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    tileColor: lightGreen,
+                    title: Text('01/01/2000---${index + 1}'),
+                    trailing: Text('-2000/-'),
+                  ),
+                );
               },
-              icon: Icon(
-                Icons.keyboard_arrow_left,
-                size: 30,
-              )),
-          backgroundColor: primaryColorBlue,
-          foregroundColor: white,
-          title: Text("History"),
-        ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
-              child: ListTile(
-                subtitle: Text("Balance Amount:1000"),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                tileColor: lightGreen,
-                title: Text('01/01/2000---${index + 1}'),
-                trailing: Text('-2000/-'),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              color: primaryColorBlue,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total Balance :',
+                    style: TextStyle(color: white),
+                  ),
+                  Text(
+                    "4000\\-",
+                    style: TextStyle(color: white),
+                  ),
+                ],
               ),
-            );
-          },
-        ));
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
