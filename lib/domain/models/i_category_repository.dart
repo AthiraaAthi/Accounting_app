3f4a96df03ec6c7c49cdc1892e8d84c3948110f3
H4r1 <harigovindappu4926@gmail.com>
2024-12-18 12:51:41 +0530

    repository function for category

---
+++ lib/domain/models/i_category_repository.dart
@@ -0,0 +1,7 @@
+import 'package:curved_nav/domain/failures/main_failure.dart';
+import 'package:curved_nav/domain/models/category%20model/category_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class ICategoryRepository {
+  Future<Either<MainFailures, List<CategoryModel>>> categoryAddAndGet();
+}
