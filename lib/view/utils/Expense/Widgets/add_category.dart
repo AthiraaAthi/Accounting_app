4c8cbea9a2e859f5dda98e749e4550f6aab435d4
H4r1 <harigovindappu4926@gmail.com>
2024-12-18 12:37:18 +0530

    new alertdialog to add category

--- lib/view/utils/Expense/Widgets/add_category.dart
+++ lib/view/utils/Expense/Widgets/add_category.dart
@@ -1,28 +1,46 @@
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
-class AddCategory extends StatelessWidget {
-  const AddCategory({super.key});
+class CategoryAddDialog extends StatelessWidget {
+  const CategoryAddDialog({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Add Category",
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.keyboard_arrow_left,
-              size: 30,
-              color: Colors.white,
-            )),
-        backgroundColor: ColorConstant.defBlue,
+    return AlertDialog(
+      backgroundColor: white,
+      title: Text('Add category'),
+      content: TextField(
+        decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: ColorConstant.defBlue, width: 2.0),
+            ),
+            hintText: "Amount",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
       ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Cancel',
+            style: TextStyle(color: black),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Add',
+            style: TextStyle(color: primaryColorBlue),
+          ),
+        )
+      ],
     );
   }
 }
