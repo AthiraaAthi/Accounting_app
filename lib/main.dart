021d45af31cd39d45c388d7e7dbcd775fe15176a
H4r1 <harigovindappu4926@gmail.com>
2024-12-22 13:12:56 +0530

    linked to firebase and added firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'package:curved_nav/Application/Expense/expense_bloc.dart';
 import 'package:curved_nav/domain/core/d_i/injectable.dart';
 import 'package:curved_nav/domain/models/Expense%20model/expense_model.dart';
 import 'package:curved_nav/domain/models/category%20model/category_model.dart';
+import 'package:curved_nav/firebase_options.dart';
 import 'package:curved_nav/view/utils/Navigation/nav_screen.dart';
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await configInjectable();
   await Hive.initFlutter();
   if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
