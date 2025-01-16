41d19faa3b5014d5053ffe0403a93f124e222715
AthiraaAthi <athiraa2612@gmail.com>
2025-01-16 12:34:07 +0530

    changed app name

--- lib/view/utils/Home/home.dart
+++ lib/view/utils/Home/home.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       appBar: AppBar(
         backgroundColor: ColorConstant.defBlue,
         title: Text(
-          "Shukkudu :)",
+          "LENDO",
           style: TextStyle(
               color: Colors.white, fontSize: 19, fontWeight: FontWeight.w600),
         ),
