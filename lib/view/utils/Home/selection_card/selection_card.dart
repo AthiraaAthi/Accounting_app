4e395710029e63b949cdac1cdd94b71efccfdd93
H4r1 <harigovindappu4926@gmail.com>
2025-01-16 14:00:52 +0530

    used stream builder instead of bloc for getting firebase items

--- lib/view/utils/Home/selection_card/selection_card.dart
+++ lib/view/utils/Home/selection_card/selection_card.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:curved_nav/Application/Lender/lender_bloc.dart';
 import 'package:curved_nav/domain/models/Lending%20Card%20model/lending_model.dart';
+import 'package:curved_nav/domain/models/history%20and%20others%20model/history_model.dart';
 import 'package:curved_nav/view/utils/Home/Widgets/calender.dart';
 import 'package:curved_nav/view/utils/Home/Widgets/infoAlert.dart';
 import 'package:curved_nav/view/utils/Home/Widgets/popUpMenu.dart';
 import 'package:curved_nav/view/utils/Home/selection_card/history_screen.dart';
 import 'package:curved_nav/view/utils/color_constant/color_constant.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,6 +25,15 @@ class SelectionCard extends StatelessWidget {
         context.read<LenderBloc>().add(History(id: state.id!));
       },
     );
+    final usedId = FirebaseAuth.instance.currentUser!.uid;
+    final snapshot = FirebaseFirestore.instance
+        .collection('users')
+        .doc(usedId)
+        .collection('lender')
+        .doc(state.id)
+        .collection('details')
+        .orderBy('timestamp', descending: true)
+        .snapshots();
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -145,88 +157,70 @@ class SelectionCard extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: BlocBuilder<LenderBloc, LenderState>(
-                builder: (context, state) {
-                  final data = state.historyData;
-                  final firstData = data[0];
-                  return ListTile(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    tileColor: lightGreen,
-                    leading: Text('01/01/2000'),
-                    trailing: Text('-${firstData.amount}/-'),
+            StreamBuilder<QuerySnapshot>(
+                stream: snapshot,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: SizedBox());
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return Center(child: Text('No data available.'));
+                  }
+                  final docs = snapshot.data!.docs;
+                  final data = docs
+                      .map((doc) => HistoryModel.fromJson(
+                          doc.data() as Map<String, dynamic>))
+                      .toList();
+                  return ListView.separated(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final items = data[index];
+
+                      return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            tileColor: lightGreen,
+                            leading: Text('01/01/2000'),
+                            trailing: Text('-${items.amount}/-'),
+                          ));
+                    },
+                    separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    ),
+                    itemCount: data.length > 4 ? 4 : data.length,
                   );
-                },
-              ),
-            ),
+                }),
             SizedBox(
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                tileColor: lightGreen,
-                leading: Text('01/01/2000'),
-                trailing: Text('-2000/-'),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                tileColor: lightGreen,
-                leading: Text('01/01/2000'),
-                trailing: Text('-2000/-'),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                tileColor: lightGreen,
-                leading: Text('01/01/2000'),
-                trailing: Text('-2000/-'),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                color: primaryColorBlue,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Balance amount:',
-                      style: TextStyle(color: white),
-                    ),
-                    Text(
-                      "4000\\-",
-                      style: TextStyle(color: white),
-                    ),
-                  ],
-                ),
-              ),
-            )
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: primaryColorBlue,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Balance amount:',
+                style: TextStyle(color: white),
+              ),
+              Text(
+                "4000\\-",
+                style: TextStyle(color: white),
+              ),
+            ],
+          ),
         ),
       ),
     );
