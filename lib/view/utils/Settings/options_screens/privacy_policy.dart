530f367bd869ae9bbb7c618864c5f31c296a4108
AthiraaAthi <athiraa2612@gmail.com>
2025-01-16 13:06:24 +0530

    enhance PrivacyPolicy layout with scrollable view and add information collection section

--- lib/view/utils/Settings/options_screens/privacy_policy.dart
+++ lib/view/utils/Settings/options_screens/privacy_policy.dart
@@ -38,25 +38,39 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Effective Date: [ Date]",
-              style: TextStyle(fontSize: 20),
-            ),
-            titleSpace,
-            Text("1. Introduction", style: pointsStyle),
-            titleSpace,
-            Text(
-              '''Welcome to LENDO. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our mobile application (the "LENDO"). By using the App, you agree to the collection and use of information in accordance with this policy. ''',
-              style: explainationStyle,
-              textAlign: TextAlign.justify,
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Effective Date: [ Date]",
+                style: TextStyle(fontSize: 20),
+              ),
+              titleSpace,
+              Text("1. Introduction", style: pointsStyle),
+              titleSpace,
+              Text(
+                '''Welcome to LENDO. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our mobile application (the "LENDO"). By using the App, you agree to the collection and use of information in accordance with this policy. ''',
+                style: explainationStyle,
+                textAlign: TextAlign.justify,
+              ),
+              nextSpace,
+              Text("2. Information We Collect", style: pointsStyle),
+              titleSpace,
+              Text(
+                '''We collect the following types of information:
+        •Personal Information: Name, phone number, email, and other necessary details to verify identity and facilitate transactions. 
+        •Financial Information: Loan amount, repayment history, and other relevant financial details.
+        •Device Information: IP address, device ID, operating system, and other technical information.
+        •Advertising Data: We may collect ad interaction data for personalized advertising experiences. ''',
+                textAlign: TextAlign.justify,
+                style: explainationStyle,
+              )
+            ],
+          ),
         ),
       ),
     );
