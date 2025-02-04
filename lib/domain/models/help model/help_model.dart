4755e1e740896c474e956e2573899137d32d3659
H4r1 <harigovindappu4926@gmail.com>
2025-02-04 12:49:16 +0530

    created new model

---
+++ lib/domain/models/help model/help_model.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'help_model.freezed.dart';
+part 'help_model.g.dart';
+
+@freezed
+class HelpModel with _$HelpModel {
+  factory HelpModel({
+    bool? IsMoneyLent,
+    String? id,
+    String? name,
+    String? phone,
+    String? problem,
+  }) = _HelpModel;
+
+  factory HelpModel.fromJson(Map<String, dynamic> json) =>
+      _$HelpModelFromJson(json);
+}
