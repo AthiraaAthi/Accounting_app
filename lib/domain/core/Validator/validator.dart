3b3eab6b16ae1f8796abb337dab58c44700dc4ec
H4r1 <harigovindappu4926@gmail.com>
2025-02-05 14:45:44 +0530

    new amount Validator

--- lib/domain/core/Validator/validator.dart
+++ lib/domain/core/Validator/validator.dart
@@ -11,4 +11,10 @@ class FieldValidators {
     MaxLengthValidator(10,
         errorText: 'Enter a valid phone number (Don\'t use +91)'),
   ]);
+  static final amountValidator = MultiValidator([
+    RequiredValidator(errorText: 'This field is required'),
+    PatternValidator(r'^\d+(\.\d{1,2})?$', errorText: 'Enter a valid amount'),
+    MaxLengthValidator(10, errorText: 'Amount is too long'),
+    MinLengthValidator(1, errorText: 'Enter a valid amount'),
+  ]);
 }
