94aef13478f22d261c523e9b6d9be39afc537c72
H4r1 <harigovindappu4926@gmail.com>
2025-02-07 11:54:07 +0530

    auto leading off

--- lib/view/utils/Home/home.dart
+++ lib/view/utils/Home/home.dart
@@ -26,6 +26,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     });
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: ColorConstant.defBlue,
         title: Text(
           "FinFloww",
