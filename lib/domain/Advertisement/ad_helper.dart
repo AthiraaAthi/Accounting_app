37bb2793cddf5e2418b32b5ffd4d2ff0d3c06dd5
H4r1 <harigovindappu4926@gmail.com>
2025-02-25 08:06:26 +0530

    fixed minor bugs

--- lib/domain/Advertisement/ad_helper.dart
+++ lib/domain/Advertisement/ad_helper.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -28,46 +29,21 @@ class AdHelper {
   }
 }
 
-class AppOpenAdManager {
-  static AppOpenAd? _appOpenAd;
-  static bool _isAdAvailable = false;
+AppOpenAd? _appOpenAd;
 
-  static void loadAd() {
-    AppOpenAd.load(
-      adUnitId: AdHelper.appOpensAdUnitId,
-      request: const AdRequest(),
-      adLoadCallback: AppOpenAdLoadCallback(
-        onAdLoaded: (ad) {
-          _appOpenAd = ad;
-          _isAdAvailable = true;
-        },
-        onAdFailedToLoad: (error) {
-          _appOpenAd = null;
-          _isAdAvailable = false;
-        },
-      ),
-    );
-  }
-
-  static void showAdIfAvailable() {
-    if (_isAdAvailable && _appOpenAd != null) {
-      _appOpenAd!.fullScreenContentCallback = FullScreenContentCallback(
-        onAdDismissedFullScreenContent: (ad) {
-          ad.dispose();
-          _appOpenAd = null;
-          _isAdAvailable = false;
-          loadAd();
-        },
-        onAdFailedToShowFullScreenContent: (ad, error) {
-          ad.dispose();
-          _appOpenAd = null;
-          _isAdAvailable = false;
-          loadAd();
-        },
-      );
-      _appOpenAd!.show();
-    } else {
-      loadAd();
-    }
-  }
+loadAd() {
+  AppOpenAd.load(
+    adUnitId: AdHelper.appOpensAdUnitId,
+    request: const AdRequest(),
+    adLoadCallback: AppOpenAdLoadCallback(
+      onAdLoaded: (ad) {
+        _appOpenAd = ad;
+        _appOpenAd!.show();
+      },
+      onAdFailedToLoad: (error) {
+        _appOpenAd!.dispose();
+        log('failed to show app open ad: $error');
+      },
+    ),
+  );
 }
