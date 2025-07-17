6ba6b39962cc18f303952320292390310f65f18d
H4r1 <harigovindappu4926@gmail.com>
2025-07-17 14:34:56 +0530

    re enabled ad

--- lib/domain/Advertisement/ad_helper.dart
+++ lib/domain/Advertisement/ad_helper.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-final bool isActive = false;
+final bool isActive = true;
 
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // return 'ca-app-pub-3811809688597776/5377280334';
-      return 'ca-app-pub-3940256099942544/6300978111'; // test ad id
+      return 'ca-app-pub-3811809688597776/5377280334';
+      // return 'ca-app-pub-3940256099942544/6300978111'; // test ad id
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -17,8 +17,8 @@ class AdHelper {
 
   static String get interstatialAdUnitId {
     if (Platform.isAndroid) {
-      // return 'ca-app-pub-3811809688597776/6029862935';
-      return 'ca-app-pub-3940256099942544/1033173712'; //test ad id
+      return 'ca-app-pub-3811809688597776/6029862935';
+      // return 'ca-app-pub-3940256099942544/1033173712'; //test ad id
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -26,8 +26,8 @@ class AdHelper {
 
   static String get appOpensAdUnitId {
     if (Platform.isAndroid) {
-      //return 'ca-app-pub-3811809688597776/5529866140';
-      return 'ca-app-pub-3940256099942544/9257395921'; //test ad id
+      return 'ca-app-pub-3811809688597776/5529866140';
+      // return 'ca-app-pub-3940256099942544/9257395921'; //test ad id
     } else {
       throw UnsupportedError('Unsupported platform');
     }
